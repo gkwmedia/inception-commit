0ac49cabcaf19f512bd16b41efe1a8c24a5c5823
gkwmedia <tgusewelle@gkwmedia.com>
2022-05-22 13:56:25 -0500

    added interface for commits api

--- src/app/services/http-client.service.ts
+++ src/app/services/http-client.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Commit } from '../interfaces/commits';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpClientService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getCommits(url: string): Observable<Commit[]> {
+    return this.http.get<Commit[]>(url);
+  }
 }
--- src/app/table-body/table-body.component.ts
+++ src/app/table-body/table-body.component.ts
@@ -1,15 +1,28 @@
+import { HttpParamsOptions } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Commit } from '../interfaces/commits';
+
+import { HttpClientService } from '../services/http-client.service';
 
 @Component({
   selector: 'app-table-body',
   templateUrl: './table-body.component.html',
-  styleUrls: ['./table-body.component.css']
+  styleUrls: ['./table-body.component.css'],
 })
 export class TableBodyComponent implements OnInit {
+  constructor(private http: HttpClientService) {}
 
-  constructor() { }
+  commits: Commit[] = [];
 
   ngOnInit(): void {
+    this.http
+      .getCommits(
+        'https://api.github.com/repos/gkwmedia/inception-commit/commits'
+      )
+      .subscribe((data) => {
+        this.commits = data;
+        console.log(data);
+      });
   }
-
 }
