9f624faa28242087fda16b7f0cb4f54fd8961872
gkwmedia <tgusewelle@gkwmedia.com>
2022-05-22 10:51:27 -0500

    added table compnents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableBodyComponent } from './table-body/table-body.component';
+import { RowComponent } from './table-body/row/row.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, TableBodyComponent, RowComponent],
   imports: [BrowserModule, HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
