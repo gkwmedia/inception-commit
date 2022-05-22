8cb97b314127a99110010f37675e453db8201a97
gkwmedia <tgusewelle@gkwmedia.com>
2022-05-22 14:47:45 -0500

    commit list left side start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TableBodyComponent } from './table-body/table-body.component';
-import { RowComponent } from './table-body/row/row.component';
+
 import { CommitListComponent } from './commit-list/commit-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, TableBodyComponent, RowComponent, CommitListComponent],
+  declarations: [AppComponent, NavbarComponent, CommitListComponent],
   imports: [BrowserModule, HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
